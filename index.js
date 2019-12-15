76456077479b965794b23307ae1399c573865dd2
NapadiyOles <44135388+NapadiyOles@users.noreply.github.com>
2019-12-15 16:08:11 +0200

    logic added

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (request, response) => {
-    response.send('Hello from Express!')
-})
+app.use(express.static(__dirname + './app'));
+
+app.get("/", function(req, res) {
+    res.sendFile('./app/index.html', {root: __dirname });
+});
 
 app.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err)
-    }
+}
 
     console.log(`server is listening on ${port}`)
 })
